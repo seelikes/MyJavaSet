4c091b2989b33f0bbb33e2bd015ba6c43963f8ed
刘甜甜 <seelikes@163.com>
2021-05-19 01:57:42 +0800

    add /most-stones-removed-with-same-row-or-column

--- test-leecode/src/test/java/com/ltt/jsp/leecode/LeeCodeTests.kt
+++ test-leecode/src/test/java/com/ltt/jsp/leecode/LeeCodeTests.kt
@@ -1075,4 +1075,27 @@ class LeeCodeTests {
     private fun trulyMostPopular(names: Array<String>, synonyms: Array<String>): Array<String> {
         return arrayOf()
     }
+
+    // 947. 移除最多的同行或同列石头
+    // https://leetcode-cn.com/problems/most-stones-removed-with-same-row-or-column/
+    // 中等
+    // status: record
+    @Test
+    fun test_removeStones() {
+        Assert.assertEquals(
+            5,
+            arrayOf(
+                intArrayOf(0, 0),
+                intArrayOf(0, 1),
+                intArrayOf(1, 0),
+                intArrayOf(1, 2),
+                intArrayOf(2, 1),
+                intArrayOf(2, 2),
+            )
+        )
+    }
+
+    private fun removeStones(stones: Array<IntArray>): Int {
+        return 0
+    }
 }
